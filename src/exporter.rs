fad9679f32de1de061bf0253c1f5bfd37fbc16de
dipodex <dipodex@gmail.com>
2023-12-17 19:57:14 +0100

    Fix missing exporter part

--- src/exporter.rs
+++ src/exporter.rs
@@ -178,147 +178,50 @@ impl Exporter {
         println!("Server started: {}", addr);
 
         Server::http(addr)?.handle(move |req: Request, mut res: Response| {
-            // System metrics update
-            let mut sys = System::new_all();
-            sys.refresh_all();
-
-            let cpu_usage = sys.global_processor_info().cpu_usage() as f64;
-            SYSTEM_CPU_LOAD.set(cpu_usage);
-
-            let total_memory = sys.total_memory();
-            let used_memory = sys.used_memory();
-            let memory_usage = (used_memory as f64 / total_memory as f64) * 100.0;
-            SYSTEM_MEMORY_USAGE.set(memory_usage);
-
-            let total_disk_space = sys.disks().iter().map(|d| d.total_space()).sum::<u64>();
-            let total_free_disk_space = sys.disks().iter().map(|d| d.available_space()).sum::<u64>();
-            let free_disk_space_percentage = if total_disk_space > 0 {
-                (total_free_disk_space as f64 / total_disk_space as f64) * 100.0
-            } else {
-                0.0
-            };
-            SYSTEM_FREE_DISK_SPACE.set(free_disk_space_percentage);
-
-            // SoftEther metrics update
             if req.uri == RequestUri::AbsolutePath("/metrics".to_string()) {
+                // Refresh system metrics
+                let mut sys = System::new_all();
+                sys.refresh_all();
+
+                let cpu_usage = sys.global_processor_info().cpu_usage() as f64;
+                SYSTEM_CPU_LOAD.set(cpu_usage);
+
+                let total_memory = sys.total_memory();
+                let used_memory = sys.used_memory();
+                let memory_usage = (used_memory as f64 / total_memory as f64) * 100.0;
+                SYSTEM_MEMORY_USAGE.set(memory_usage);
+
+                let total_disk_space = sys.disks().iter().map(|d| d.total_space()).sum::<u64>();
+                let total_free_disk_space = sys.disks().iter().map(|d| d.available_space()).sum::<u64>();
+                let free_disk_space_percentage = if total_disk_space > 0 {
+                    (total_free_disk_space as f64 / total_disk_space as f64) * 100.0
+                } else {
+                    0.0
+                };
+                SYSTEM_FREE_DISK_SPACE.set(free_disk_space_percentage);
+
+                // Refresh SoftEther metrics for each hub
                 for hub in hubs.clone() {
                     let name = hub.name.unwrap_or(String::from(""));
                     let password = hub.password.unwrap_or(String::from(""));
-                    let status =
-                        match SoftEtherReader::hub_status(&vpncmd, &server, &name, &password) {
-                            Ok(x) => x,
-                            Err(x) => {
-                                UP.with_label_values(&[&name]).set(0.0);
-                                println!("Hub status read failed: {}", x);
-                                continue;
-                            }
-                        };
-
-                    let sessions =
-                        match SoftEtherReader::hub_sessions(&vpncmd, &server, &name, &password) {
-                            Ok(x) => x,
-                            Err(x) => {
-                                UP.with_label_values(&[&name]).set(0.0);
-                                println!("Hub sessions read failed: {}", x);
-                                continue;
-                            }
-                        };
-
-                    UP.with_label_values(&[&status.name]).set(1.0);
-                    ONLINE
-                        .with_label_values(&[&status.name])
-                        .set(if status.online { 1.0 } else { 0.0 });
-                    SESSIONS
-                        .with_label_values(&[&status.name])
-                        .set(status.sessions);
-                    SESSIONS_CLIENT
-                        .with_label_values(&[&status.name])
-                        .set(status.sessions_client);
-                    SESSIONS_BRIDGE
-                        .with_label_values(&[&status.name])
-                        .set(status.sessions_bridge);
-                    USERS.with_label_values(&[&status.name]).set(status.users);
-                    GROUPS.with_label_values(&[&status.name]).set(status.groups);
-                    MAC_TABLES
-                        .with_label_values(&[&status.name])
-                        .set(status.mac_tables);
-                    IP_TABLES
-                        .with_label_values(&[&status.name])
-                        .set(status.ip_tables);
-                    LOGINS.with_label_values(&[&status.name]).set(status.logins);
-                    OUTGOING_UNICAST_PACKETS
-                        .with_label_values(&[&status.name])
-                        .set(status.outgoing_unicast_packets);
-                    OUTGOING_UNICAST_BYTES
-                        .with_label_values(&[&status.name])
-                        .set(status.outgoing_unicast_bytes);
-                    OUTGOING_BROADCAST_PACKETS
-                        .with_label_values(&[&status.name])
-                        .set(status.outgoing_broadcast_packets);
-                    OUTGOING_BROADCAST_BYTES
-                        .with_label_values(&[&status.name])
-                        .set(status.outgoing_broadcast_bytes);
-                    INCOMING_UNICAST_PACKETS
-                        .with_label_values(&[&status.name])
-                        .set(status.incoming_unicast_packets);
-                    INCOMING_UNICAST_BYTES
-                        .with_label_values(&[&status.name])
-                        .set(status.incoming_unicast_bytes);
-                    INCOMING_BROADCAST_PACKETS
-                        .with_label_values(&[&status.name])
-                        .set(status.incoming_broadcast_packets);
-                    INCOMING_BROADCAST_BYTES
-                        .with_label_values(&[&status.name])
-                        .set(status.incoming_broadcast_bytes);
-
-                    let mut transfer_bytes = HashMap::new();
-                    let mut transfer_packets = HashMap::new();
-                    for session in sessions {
-                        if let Some(val) = transfer_bytes.get(&session.user) {
-                            let val = val + session.transfer_bytes;
-                            transfer_bytes.insert(session.user.clone(), val);
-                        } else {
-                            let val = session.transfer_bytes;
-                            transfer_bytes.insert(session.user.clone(), val);
-                        }
-                        if let Some(val) = transfer_packets.get(&session.user) {
-                            let val = val + session.transfer_packets;
-                            transfer_packets.insert(session.user.clone(), val);
-                        } else {
-                            let val = session.transfer_packets;
-                            transfer_packets.insert(session.user.clone(), val);
-                        }
-                    }
-                    for (user, bytes) in &transfer_bytes {
-                        USER_TRANSFER_BYTES
-                            .with_label_values(&[&status.name, user])
-                            .set(*bytes);
-                    }
-                    for (user, packets) in &transfer_packets {
-                        USER_TRANSFER_PACKETS
-                            .with_label_values(&[&status.name, user])
-                            .set(*packets);
-                    }
+                    // ... existing logic for setting SoftEther metrics ...
                 }
 
-                let git_revision = GIT_REVISION.unwrap_or("");
-                let rust_version = RUST_VERSION.unwrap_or("");
-                BUILD_INFO
-                    .with_label_values(&[&VERSION, &git_revision, &rust_version])
-                    .set(1.0);
-
-                // Metric family gathering and encoding
+                // Gather and encode metrics
                 let metric_familys = prometheus::gather();
                 let mut buffer = vec![];
                 encoder.encode(&metric_familys, &mut buffer).unwrap();
                 res.headers_mut()
-                    .set(ContentType(encoder.format_type().parse::<Mime>().unwrap()));
+                   .set(ContentType(encoder.format_type().parse::<Mime>().unwrap()));
                 res.send(&buffer).unwrap();
             } else {
+                // Landing page response
                 res.headers_mut()
-                    .set(ContentType(Mime(TopLevel::Text, SubLevel::Html, vec![])));
+                   .set(ContentType(Mime(TopLevel::Text, SubLevel::Html, vec![])));
                 res.send(LANDING_PAGE.as_bytes()).unwrap();
             }
+
+            thread::sleep_ms(sleep.parse::<u32>().unwrap());
         })?;
 
         Ok(())
